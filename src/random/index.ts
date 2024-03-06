c7fb63c12a1b95ee0e70bee3fb5d05789c2c0968
liuxspro <121993326+liuxspro@users.noreply.github.com>
2024-03-06 23:51:28 +0800

    Chore: add random function

---
+++ src/random/index.ts
@@ -0,0 +1,10 @@
+function random(minNum: number, maxNum?: number) {
+    // 生成[n,m]的随机整数
+    let by_min = Math.random() * minNum + 1;
+    if (maxNum) {
+        let by_min_and_max = Math.random() * (maxNum - minNum + 1) + minNum;
+        return parseInt(by_min_and_max.toString(), 10);
+    } else {
+        return parseInt(by_min.toString(), 10);
+    }
+}
