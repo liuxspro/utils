393878b54064ede09166ddc7158bd3ac834ab8df
liuxspro <liuxspro@gmail.com>
2024-07-10 11:48:22 +0800

    Fix: type missing

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,5 +1,5 @@
 /** 获取某个数字的整数部分位数 */
-export function get_digits(n: number) {
+export function get_digits(n: number): number {
   const integerPart = Math.floor(Math.abs(n)); // 取绝对值并向下取整
   return integerPart.toString().length;
 }
