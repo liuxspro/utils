76eca19f73e875449e01fa89f696fbee1346c7d0
liuxspro <liuxspro@gmail.com>
2025-01-27 14:59:49 +0800

    Chore: move

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export * from "./math/index.ts";
-export * from "./esri_wkt.ts";
+export * from "./gis/esri_wkt.ts";
 export * from "./gis/xyzmap.ts";
 export * from "./filetype.ts";
+
+/** 判断两个 array 是否相等 */
+export function array_is_equal(a: Uint8Array, b: Uint8Array): boolean {
+  if (a.length !== b.length) return false;
+  return a.every((value, index) => value === b[index]);
+}
